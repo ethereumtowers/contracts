1a1ea327ee3f5fe7ef21df1cb6969e83f63a0b25
avasilevichaet <artsiom.vasilevich@aetsoft.net>
2022-06-28 18:24:38 +0300

    update deployment scripts

--- scripts/deployStaking.js
+++ scripts/deployStaking.js
@@ -7,7 +7,7 @@ async function deployStakingV2() {
 
   const stakingContractFactory = await hre.ethers.getContractFactory('EthereumWorldsNFTStaking');
   const staking = await stakingContractFactory.deploy(
-    "0xEBF2b3F77022fB9b45C400E8597E55e207973144",
+    "0xDDF74cB0202080c83EbE91c4345af622A3e5e4DA",
     "0x1944937643d8317D23bc49766412385F8ec6452F",
     "0xf11b2cE99AF0506317f2EE28ADb47ee94B08Cd02",
   );
